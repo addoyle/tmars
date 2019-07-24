68cd808015c61fad1dfa9a0696c4bf1f31e538d5
Andy Doyle <andy.doyle@inovexcorp.com>
2019-07-24 07:32:26 -0400

    Switching from em to px

--- src/client/components/board/GlobalParameters.js
+++ src/client/components/board/GlobalParameters.js
@@ -10,45 +10,45 @@ export default class GlobalParameters extends Component {
     return (
       <div className="global-params">
         <div className="flex">
-          <div className="col-1 temp-wrapper flex">
-            <Param name="temperature" />
-            <div
-                className={`temp-gauge bottom ${this.props.temperature === 8 ? 'maxed' : ''}`}
-                style={{height: `${((this.props.temperature + 30) / 3.8 + .25)}em`}}>
-              <span>{`${this.props.temperature > 0 ? '+' : ''}${this.props.temperature}°C`}</span>
-            </div>
-
-            <div className={`bonus ${this.props.temperature >= -24 ? 'met' : ''}`} style={{top: `${10 - (6 / 3.8)}em`}} data-content="-24">
-              <Production>
-                <div className="flex">
-                  <Resource name="heat" />
-                </div>
-              </Production>
-            </div>
-
-            <div className={`bonus ${this.props.temperature >= -20 ? 'met' : ''}`} style={{top: `${10 - (10 / 3.8)}em`}} data-content="-20">
-              <Production>
-                <div className="flex">
-                  <Resource name="heat" />
-                </div>
-              </Production>
-            </div>
-
-            <div className={`bonus ${this.props.temperature >= 0 ? 'met' : ''}`} style={{top: `${10 - (30 / 3.8)}em`}} data-content="0">
-              <Tile name="ocean" />
-            </div>
-          </div>
-
           <div className="col-1 oxy-wrapper flex">
             <Param name="oxygen" />
             <div
                 className={`oxy-gauge bottom ${this.props.oxygen === 14 ? 'maxed' : ''}`}
-                style={{height: `${(this.props.oxygen / 1.4 + .25)}em`}}>
+                style={{height: `${(this.props.oxygen / (14 / 329) + 9)}px`}}>
               <span>{`${this.props.oxygen}%`}</span>
             </div>
 
-            <div className={`bonus ${this.props.oxygen >= 8 ? 'met' : ''}`} style={{top: `${10 - (8 / 1.4)}em`}} data-content="8%">
+            <div className={`bonus ${this.props.oxygen >= 8 ? 'met' : ''}`} style={{top: `${329 - (8 / (14 / 329))}px`}} data-content="8%">
               <Param name="temperature" />
+            </div>
+          </div>
+
+          <div className="col-1 temp-wrapper flex">
+            <Param name="temperature" />
+            <div
+                className={`temp-gauge bottom ${this.props.temperature === 8 ? 'maxed' : ''}`}
+                style={{height: `${((this.props.temperature + 30) / (38 / 329) + 9)}px`}}>
+              <span>{`${this.props.temperature > 0 ? '+' : ''}${this.props.temperature}°C`}</span>
+            </div>
+
+            <div className={`bonus ${this.props.temperature >= -24 ? 'met' : ''}`} style={{top: `${329 - (6 / (38 / 329))}px`}} data-content="-24">
+              <Production>
+                <div className="flex">
+                  <Resource name="heat" />
+                </div>
+              </Production>
+            </div>
+
+            <div className={`bonus ${this.props.temperature >= -20 ? 'met' : ''}`} style={{top: `${329 - (10 / (38 / 329))}px`}} data-content="-20">
+              <Production>
+                <div className="flex">
+                  <Resource name="heat" />
+                </div>
+              </Production>
+            </div>
+
+            <div className={`bonus ${this.props.temperature >= 0 ? 'met' : ''}`} style={{top: `${329 - (30 / (38 / 329))}px`}} data-content="0">
+              <Tile name="ocean" />
             </div>
           </div>
         </div>
