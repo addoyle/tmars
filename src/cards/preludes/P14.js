93a58eca93f36f07af97edf588ee69e7f1cd8af0
adoyle <adoyle@clearedgeit.com>
2020-02-10 00:34:27 -0500

    Adding more preludes

---
+++ src/cards/preludes/P14.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Prelude from '../Prelude';
+import { Tile } from '../../client/components/assets/Assets';
+
+const desc = 'Place 2 ocean tiles.';
+
+export default new Prelude({
+  number: 'P14',
+  title: 'Great Aquifer',
+  tags: [],
+  set: 'prelude',
+  desc,
+  flavor: 'You found a big one!',
+  emoji: '🌊️',
+  layout: (
+    <div className="flex gutter">
+      <div className="col-1 text-center middle">
+        <div className="resources">
+          <Tile name="ocean" />
+          <Tile name="ocean" />
+        </div>
+      </div>
+      <div className="col-2 description middle">{desc}</div>
+    </div>
+  )
+});
