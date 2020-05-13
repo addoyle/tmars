07130ce141d56bc85d86c491a0bf192521f22558
Andy Doyle <andy.doyle@inovexcorp.com>
2020-05-13 13:37:49 -0400

    Adding card placeholders

---
+++ src/shared/cards/project/246.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Event from '../Event';
+import {
+  Resource,
+  Param,
+  Tag
+} from '../../../client/game/components/assets/Assets';
+
+const desc =
+  'Raise Venus 1 step. Add 1 floater to A VENUS CARD for each Jovian tag you have.';
+
+export default new Event({
+  number: 246,
+  title: 'Hydrogen To Venus',
+  cost: 11,
+  tags: ['space', 'event'],
+  set: 'venus',
+  desc,
+  flavor:
+    'Easily collected from the gas giants, hydrogen can increase floating power, or be converted to precious water',
+  clientAction: () => {},
+  serverAction: () => {},
+  emoji: '🌀',
+  layout: (
+    <div className="flex">
+      <div className="col-2 middle">
+        <div className="resources">
+          <Param name="venus" />
+        </div>
+        <div className="resources">
+          <Resource name="floater" tag="venus" />
+          <span>/</span>
+          <Tag name="jovian" />
+        </div>
+      </div>
+      <div className="col-3 middle description">{desc}</div>
+    </div>
+  )
+});
---
+++ src/shared/cards/project/247.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Automated from '../Automated';
+import {
+  VictoryPoint,
+  Tag,
+  Param
+} from '../../../client/game/components/assets/Assets';
+
+const desc = 'Draw 1 card, or draw 3 cards if you have at least 3 Venus tags.';
+
+export default new Automated({
+  number: 247,
+  title: 'IO Sulphur Reasearch',
+  cost: 17,
+  tags: ['science', 'jovian'],
+  set: 'venus',
+  desc,
+  flavor: 'Finding new uses for all the suphur coming out of the Venus venture',
+  clientAction: () => {},
+  serverAction: () => {},
+  vp: 2,
+  emoji: '🔬',
+  layout: (
+    <div className="m-top">
+      <div className="resources text-center">
+        <Param name="card back" />
+        <span> OR </span>
+        <Tag name="venus" style={{ marginRight: '-.5em', zIndex: 2 }} />
+        <Tag name="venus" style={{ marginRight: '-.5em', zIndex: 1 }} />
+        <Tag name="venus" />
+        <span>:</span>
+        <Param name="card back" style={{ marginRight: '-.5em' }} />
+        <Param
+          name="card back"
+          style={{ marginRight: '-.5em', marginTop: '-.25em' }}
+        />
+        <Param name="card back" />
+      </div>
+      <div className="flex gutter">
+        <div className="description middle text-center">{desc}</div>
+        <div className="text-right bottom">
+          <VictoryPoint>
+            <span className="big point">2</span>
+          </VictoryPoint>
+        </div>
+      </div>
+    </div>
+  )
+});
