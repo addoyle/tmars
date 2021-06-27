c8ab471464493cf4d5d71d820b21b57e11fc7d2e
Andy Doyle <andy.doyle@inovexcorp.com>
2021-06-27 10:36:56 -0400

    Fixing typo in constants

--- src/shared/game/constants.js
+++ src/shared/game/constants.js
@@ -1,64 +1,64 @@
 // Standard resources
-export const ES_MEGACREDIT = 'megacredit';
-export const ES_STEEL = 'steel';
-export const ES_TITANIUM = 'titanium';
-export const ES_PLANT = 'plant';
-export const ES_POWER = 'power';
-export const ES_HEAT = 'heat';
+export const RES_MEGACREDIT = 'megacredit';
+export const RES_STEEL = 'steel';
+export const RES_TITANIUM = 'titanium';
+export const RES_PLANT = 'plant';
+export const RES_POWER = 'power';
+export const RES_HEAT = 'heat';
 
 // Other resources
-export const ES_SCIENCE = 'science';
-export const ES_MICROBE = 'microbe';
-export const ES_ANIMAL = 'animal';
-export const ES_FIGHTER = 'fighter';
-export const ES_ASTEROID = 'asteroid';
-export const ES_FLOATER = 'floater';
-export const ES_DISEASE = 'disease';
-export const ES_ALL = 'all';
+export const RES_SCIENCE = 'science';
+export const RES_MICROBE = 'microbe';
+export const RES_ANIMAL = 'animal';
+export const RES_FIGHTER = 'fighter';
+export const RES_ASTEROID = 'asteroid';
+export const RES_FLOATER = 'floater';
+export const RES_DISEASE = 'disease';
+export const RES_ALL = 'all';
 
 // Tags
-export const AG_BUILDING = 'building';
-export const AG_SPACE = 'space';
-export const AG_POWER = 'power';
-export const AG_SCIENCE = 'science';
-export const AG_JOVIAN = 'jovian';
-export const AG_EARTH = 'earth';
-export const AG_PLANT = 'plant';
-export const AG_MICROBE = 'microbe';
-export const AG_ANIMAL = 'animal';
-export const AG_CITY = 'city';
-export const AG_EVENT = 'event';
-export const AG_VENUS = 'venus';
-export const AG_ALL = 'all';
-export const AG_ANY = 'any';
+export const TAG_BUILDING = 'building';
+export const TAG_SPACE = 'space';
+export const TAG_POWER = 'power';
+export const TAG_SCIENCE = 'science';
+export const TAG_JOVIAN = 'jovian';
+export const TAG_EARTH = 'earth';
+export const TAG_PLANT = 'plant';
+export const TAG_MICROBE = 'microbe';
+export const TAG_ANIMAL = 'animal';
+export const TAG_CITY = 'city';
+export const TAG_EVENT = 'event';
+export const TAG_VENUS = 'venus';
+export const TAG_ALL = 'all';
+export const TAG_ANY = 'any';
 
 // Tiles
-export const ILE_OCEAN = 'ocean';
-export const ILE_CITY = 'city';
-export const ILE_GREENERY = 'greenery';
+export const TILE_OCEAN = 'ocean';
+export const TILE_CITY = 'city';
+export const TILE_GREENERY = 'greenery';
 
 // Special tiles
-export const ILE_CAPITAL = 'capital city';
-export const ILE_NATURAL_PRESERVE = 'mars';
-export const ILE_MINING_AREA = 'mine';
-export const ILE_MINING_RIGHTS = 'mine';
-export const ILE_COMMERCIAL_DISTRICT = 'euro';
-export const ILE_NUCLEAR_ZONE = 'nuclear';
-export const ILE_INDUSTRIAL_CENTER = 'factory';
-export const ILE_ECOLOGICAL_ZONE = 'animal';
-export const ILE_LAVA_FLOWS = 'volcano';
-export const ILE_MOHOLE_AREA = 'mohole';
-export const ILE_RESTRICTED_AREA = 'restricted';
-export const ILE_DEIMOS_DOWN = 'deimos';
-export const ILE_GREAT_DAM = 'dam';
-export const ILE_MAGNETIC_FIELD_GENERATORS = 'magnet';
+export const TILE_CAPITAL = 'capital city';
+export const TILE_NATURAL_PRESERVE = 'mars';
+export const TILE_MINING_AREA = 'mine';
+export const TILE_MINING_RIGHTS = 'mine';
+export const TILE_COMMERCIAL_DISTRICT = 'euro';
+export const TILE_NUCLEAR_ZONE = 'nuclear';
+export const TILE_INDUSTRIAL_CENTER = 'factory';
+export const TILE_ECOLOGICAL_ZONE = 'animal';
+export const TILE_LAVA_FLOWS = 'volcano';
+export const TILE_MOHOLE_AREA = 'mohole';
+export const TILE_RESTRICTED_AREA = 'restricted';
+export const TILE_DEIMOS_DOWN = 'deimos';
+export const TILE_GREAT_DAM = 'dam';
+export const TILE_MAGNETIC_FIELD_GENERATORS = 'magnet';
 
 // Global parameters
-export const ARAM_TEMPERATURE = 'temperature';
-export const ARAM_OXYGEN = 'oxygen';
+export const PARAM_TEMPERATURE = 'temperature';
+export const PARAM_OXYGEN = 'oxygen';
 // Omitting Ocean as it is typically referred to by the tile
-export const ARAM_VENUS = 'venus';
+export const PARAM_VENUS = 'venus';
 
 // Other ones
-export const ERRAFORM_RATING = 'tr';
-export const ARD_BACK = 'card back';
+export const TERRAFORM_RATING = 'tr';
+export const CARD_BACK = 'card back';
